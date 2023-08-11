82fcb1c053f5753b749b50209ce120a42b07eb8b
sandy2108 <dhonisanjay116@gmail.com>
2023-08-11 11:12:37 +0530

    Modified the MetaData of OG Image

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,12 @@ import { Fragment } from 'react';
 
 
 export const metadata = {
-  title: 'MEMEPOTS | $POT',
+  title: 'MEMEPOT | $POT',
   description: 'MemePot gives away free ETH every day to pump your bags, build and grow the community, and to promote decentralized innovation.',
+  imageUrl:'https://firebasestorage.googleapis.com/v0/b/wizard-4fa3a.appspot.com/o/Memepot-logo-green.png?alt=media&token=1b410bc1-e032-46e9-8a24-dc485a06c2d0',
 };
 
-const URL = 'https://firebasestorage.googleapis.com/v0/b/wizard-4fa3a.appspot.com/o/Memepot-logo-green.png?alt=media&token=1b410bc1-e032-46e9-8a24-dc485a06c2d0';
+
 
 export default function RootLayout({ children }) {
   return (
@@ -22,12 +23,12 @@ export default function RootLayout({ children }) {
         {/* Open Graph metadata */}
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={URL} /> {/* Add OG image tag */}
+        <meta property="og:image" content={metadata.imageUrl} /> {/* Add OG image tag */}
 
         {/* Twitter Card metadata */}
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={URL} /> {/* Add Twitter image tag */}
+        <meta name="twitter:image" content={metadata.imageUrl} /> {/* Add Twitter image tag */}
       </Head>
       
       <body>
