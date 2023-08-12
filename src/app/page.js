0f7e1663588fb8dcd73379aee5c185bb5accb972
sandy2108 <dhonisanjay116@gmail.com>
2023-08-13 00:17:32 +0530

    Included the Audit component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import dynamic from 'next/dynamic';
 
+
 const Hero = dynamic(() => import('./Components/Hero'));
 const Welcome = dynamic(() => import('./Components/Welcome'));
 const Memepots = dynamic(() => import('./Components/Memepots'));
 const Insights = dynamic(() => import('./Components/Insights'));
 const Tokenomics = dynamic(() => import('./Components/Tokenomics'));
+const Audits = dynamic(()=> import('./Components/Audits'));
 
 export default function Home() {
   return (
@@ -14,6 +16,7 @@ export default function Home() {
       <Memepots />
       <Insights />
       <Tokenomics />
+      <Audits/>
     </main>
   );
 }
